feaccb14463194ce7eb0bf65090cf2c975e19d1b
vyvir <135277623+vyvir@users.noreply.github.com>
2024-08-04 11:13:32 +0300

    Create build_rpm.sh

---
+++ build_rpm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit
+
+if [ -d "./AltLinux/usr/lib" ]; then
+    rm -rf "./AltServer/usr/lib"
+fi
+if [ -d "./dist" ]; then
+    rm -rf "./dist"
+fi
+
+pyinstaller altlinux.spec --clean
+cp -R ./resources ./dist/altlinux
+mkdir -p "./AltLinux/usr/lib"
+
+cp -R ./dist/altlinux ./AltLinux/usr/lib
+chmod -R 0775 AltLinux
